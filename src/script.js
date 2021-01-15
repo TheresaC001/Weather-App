4af5fa1f1e5edc28d0d9bfe4a5f918ef7a0cd006
TheresaC001 <75188934+TheresaC001@users.noreply.github.com>
2021-01-14 19:03:58 -0500

    Removed day forecast
    started the prep for hourly forecast change

--- src/script.js
+++ src/script.js
@@ -15,6 +15,7 @@ function formatDay(date) {
   return `${days[dayIndex]}`;
 }
 
+
 function displayWeatherCondition(response) {
    document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
@@ -26,6 +27,22 @@ function searchCity(city) {
   let apiKey = "2fcf9ddeade38f382171bf594220d6e3";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayWeatherCondition);
+
+  apiURL=`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response){
+  let forecastElement =document.querySelector("#forecast");
+  forecastElement.innerhtml= ` 
+    <div class="col-2">
+        <h3> 12:00 </h3>
+          <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"
+          alt=""
+          /> 
+          <div class="weather-forecast=temperature">
+            <strong>${Math.round(forecast.main.temp_max)}°</strong> ${Math.round(forecast.main.temp_min)}°
+                    `;
 }
 
 function handleSubmit(event) {
